0730abf211b247cc8c1a4a13a2c268a94877fdc9
Raymond Liu <liura@oregonstate.edu>
2018-08-08 09:37:12 -0700

    Minor changes

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -37,11 +37,11 @@ bool visible(vector<Object*> *list, Object* light, const Vec &startpoint, const 
 	for (int i = 0; i < list->size(); i++)
 	{
 		if (list->at(i)->hit(r, 1e-3, tempmax, rec))
-			return false;
-		/*{
+			//return false;
+		{
 			if (list->at(i) != light)
 				return false;
-		}*/
+		}
 	}
 
 	return true;
@@ -63,7 +63,8 @@ Vec lightsampling(const hit_record &rec, vector<Object*> *list, const vector<Obj
 				float wi = lightDir.dot(rec.normal);
 				if (wi > 0)
 				{
-					float cos_a_max = sqrt(1 - 3 * 3 / (rec.p - lightPos).dot(rec.p - lightPos));
+					float rad = (temp->box.bounds[1] - temp->box.bounds[0]) / 2;
+					float cos_a_max = sqrt(1 - rad * rad / (rec.p - lightPos).dot(rec.p - lightPos));
 					float omega = 2/* * M_PI*/ * (1 - cos_a_max);
 					lightSampling += temp->mat->emittance * wi * omega/* * M_1_PI*/;
 				}
@@ -113,7 +114,7 @@ Vec radiance(const Ray &r, Octree *tree, int depth)
 		tree->hit(r, list);
 		if (list->size() == 0) return Vec();
 	}
-
+	
 	// GET THE LIGHTS
 	vector<Object*> lights;
 	for (Object* temp : tree->triangles)
@@ -194,8 +195,8 @@ int main()
 
 	for (int i = 0; i < meshes.size(); i++)
 	{
-		for (int j = 0; j < meshes.at(i)->triangles.size(); j++)
-			objList.push_back(meshes.at(i)->triangles.at(j));
+	for (int j = 0; j < meshes.at(i)->triangles.size(); j++)
+	objList.push_back(meshes.at(i)->triangles.at(j));
 	}*/
 
 	objList.push_back(new Sphere(Vec(), 1, new Lambertian(Vec(1, 1, 1))));
@@ -206,7 +207,7 @@ int main()
 
 	objList.push_back(new Sphere(Vec(0, -1e3 - 1, 0), 1e3, new Lambertian(Vec(0.8, 0.6, 0.2))));
 
-	objList.push_back(new Sphere(Vec(4, 8, 0), 3, /* edit lightsampling if changed radius */new Lambertian(Vec(1, 1, 1), Vec(8, 8, 8))));
+	objList.push_back(new Sphere(Vec(4, 8, 0), 2, new Lambertian(Vec(1, 1, 1), Vec(8, 8, 8))));
 
 	//BVH *root = new BVH(objList, 0);
 	Octree *root = new Octree(objList, 0);
@@ -222,11 +223,11 @@ int main()
 
 #pragma omp parallel for schedule(dynamic) num_threads(8)
 
-    for (int j = ny-1; j >= 0; j--)
-    {
+	for (int j = ny - 1; j >= 0; j--)
+	{
 		fprintf(stderr, "\rRendering : %d spp : %5.2f%%", ns, 100.*(ny - j - 1) / (ny - 1));
-        for (int i = 0; i < nx; i++)
-        {
+		for (int i = 0; i < nx; i++)
+		{
 			Vec color(0, 0, 0);
 			for (int k = 0; k < ns; k++)
 			{
@@ -246,14 +247,14 @@ int main()
 				//float v = float(j + (rand() / (RAND_MAX + 1.0))) / float(ny);
 				Ray r = cam.get_ray(u, v);
 				color += radiance(r, root, 0);
-			
+
 			}
 			color /= float(ns);
 			color = Vec(pow(clamp(color.e[0]), 1.0 / 2.2), pow(clamp(color.e[1]), 1.0 / 2.2), pow(clamp(color.e[2]), 1.0 / 2.2));
 
 			c[(ny - j - 1)*nx + i] = Vec(int(255.99*color.e[0]), int(255.99*color.e[1]), int(255.99*color.e[2]));
-	    }
-    }
+		}
+	}
 
 	ofstream myfile;
 	myfile.open("output.ppm");
@@ -262,16 +263,16 @@ int main()
 	for (int i = 0; i < nx*ny; i++)
 		myfile << c[i].e[0] << " " << c[i].e[1] << " " << c[i].e[2] << "\n";
 
-    myfile.close();
+	myfile.close();
 
 	int stop_time = clock();
-	
+
 	cout << "\n>> Running time : " << (stop_time - start_time) / 1000.0 << endl;
 	cout << ">> Objects : " << objList.size() << endl;
 
 	cin.get();
 
-    return 0;
+	return 0;
 }
 
 // PREVIOUS RADIANCE FUNCTIONS
